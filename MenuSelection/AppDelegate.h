59fc38f7c75bbbb03fd19937d08b6777e89e5e51
Keyan Bagheri <keyanbagheri@gmail.com>
2017-03-23 14:39:19 +0800

    Initial Commit

---
+++ MenuSelection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MenuSelection
+//
+//  Created by bitbender on 3/23/17.
+//  Copyright © 2017 Key. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MenuSelection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MenuSelection
+//
+//  Created by bitbender on 3/23/17.
+//  Copyright © 2017 Key. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
